b46a70f1ac3a78402a144c1815e0cf30e96e2628
Xuanyu_Li_Stat <54199678+Xuanyu-Li@users.noreply.github.com>
2022-12-08 20:50:19 +0800

    Add files via upload

--- make_map.py
+++ make_map.py
@@ -6,44 +6,35 @@ import networkx as nx
 import os
 import csv
 import pickle
-from gtfparse import read_gtf
+
 import random
-tsv_file='string_9606_ENSG_ENSP_10_all_T.tsv'
+tsv_file='./datastore/string_9606_ENSG_ENSP_10_all_T.tsv'
 
 # readinag given tsv file
 csv_table=pd.read_table(tsv_file,sep='\t')
 
 # converting tsv file into csv
-csv_table.to_csv('string_9606_ENSG_ENSP_10_all_T.csv',index=False)
+csv_table.to_csv('./datastore/string_9606_ENSG_ENSP_10_all_T.csv',index=False)
 
 # output
 print("Successfully made csv file")
 
 ensmap = {}
-# for index, row in df.iterrows():
-#     if row['gene_id'] in ensg_map.keys():
-#         ensmap[row['protein_id']] = ensg_map[row['gene_id']]
 
-from gtfparse import read_gtf
 
-savefile = "./data" + "/datastore/ensp_ensg_df.pkl"
+savefile = "./datastore/ensp_ensg_df.pkl"
 
-# If df is already stored, return the corresponding dictionary
-if os.path.isfile(savefile):
-    f = open(savefile, 'rb')
-    df = pickle.load(f)
-    f.close()
-else:
-    df = read_gtf("./data" + "/datastore/Homo_sapiens.GRCh38.95.gtf")
-    df = df[df['protein_id'] != ''][['gene_id', 'protein_id']].drop_duplicates()
-    df.to_pickle(savefile)
+
+f = open(savefile, 'rb')
+df = pickle.load(f)
+f.close()
 
 for index, row in df.iterrows():
     ensmap[row['protein_id']] = row['gene_id']
 
 
 print(" reading self.proteinlinks")
-edges = pd.read_csv("9606.protein.links.detailed.v11.5.txt", sep=' ')
+edges = pd.read_csv("./datastore/9606.protein.links.detailed.v11.5.txt", sep=' ')
 
 selected_edges = edges["coexpression"] != 0
 
